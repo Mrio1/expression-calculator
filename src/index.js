6d1481f0682e3154b6765d020bcbe333b6a69f10
Aliaksandr Zhur <sanchesmrio1@gmail.com>
2020-03-22 18:47:27 +0300

    implement 'expression calculator' task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+
 function eval() {
     // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let bracketsLeft = 0;
+    let bracketsRight = 0;
+    let arr = expr.split('').filter(element => element != " ");
+    
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == '0'){
+            if(arr[i - 1] == '/'){
+                throw ("TypeError: Division by zero.")
+            }
+        } else if (arr[i] == '('){
+            bracketsLeft += 1;
+        } else if (arr[i] == ')'){
+            bracketsRight += 1;
+        }
+    }
+        
+    if (bracketsLeft == bracketsRight) {
+        return (new Function(`return ${expr}`))();
+    } else {
+        throw ("ExpressionError: Brackets must be paired");
+    }
 }
 
 module.exports = {
